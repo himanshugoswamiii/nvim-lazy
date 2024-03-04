9682cc7e50000bb055afff7f6e2274986efba2e5
Himanshu <himanshugoswami.12feb@gmail.com>
2024-03-05 01:45:52 +0530

    LazyVim : Basic Configuration

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- local opts = { noremap = true, silent = false }
+-- silent=true won't show commands mapped to keybindings
+
+--   GOTO
+vim.keymap.set("n", "gh", "0", { noremap = true, silent = false, desc = "Goto line start" })
+vim.keymap.set("n", "gl", "$", { noremap = true, silent = false, desc = "Goto line end" })
